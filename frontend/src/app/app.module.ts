ddfff593a6f79e843e83b25f75a9ebc8d9ae6c9d
Yannick Raats <yannickraats@gmail.com>
2021-12-28 13:44:06 +0100

    house list component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,15 +8,19 @@ import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { MainComponent } from './pages/main/main.component';
+import { HouseListComponent } from './components/house-list/house-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainComponent,
     RegisterComponent,
     LoginComponent,
     NotFoundComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    HouseListComponent
   ],
   imports: [
     BrowserModule,
