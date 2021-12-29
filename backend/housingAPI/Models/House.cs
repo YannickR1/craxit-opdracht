5236ace17596290cf66ce3dca256c43ba71ffc91
Yannick Raats <yannickraats@gmail.com>
2021-12-29 12:58:26 +0100

    long to int (delete houses fix)

--- backend/housingAPI/Models/House.cs
+++ backend/housingAPI/Models/House.cs
@@ -5,7 +5,7 @@ namespace HousingAPI.Models
 {
     public partial class House
     {
-        public long Id { get; set; }
+        public int Id { get; set; }
         public string? Name { get; set; }
         public string? City { get; set; }
         public long? Rooms { get; set; }
